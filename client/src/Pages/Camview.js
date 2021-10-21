5d043be939a35ab01d160434ec58e2283dbfee6a
Kennethcole5 <kenneth.cole@selu.edu>
2021-10-20 19:35:32 -0500

    recent push

--- client/src/Pages/Camview.js
+++ client/src/Pages/Camview.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 
+
+<h1>Video Feed</h1>
+
 const Cam = () => {
     return (
-        <div>
+        <div className="camera-feed">
             <img
                 src="http://localhost:5000/video_feed"
                 alt="Video"
