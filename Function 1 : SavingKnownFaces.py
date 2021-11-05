3440585144f10245b32f85c42e8fabf01fa81f19
Ken <31672977+kennethcole5@users.noreply.github.com>
2021-11-05 18:25:39 -0500

    Updated

--- Function 1 : SavingKnownFaces.py
+++ Function 1 : SavingKnownFaces.py
@@ -1,9 +1,11 @@
 # Things to know
 # This is the documentation for the function from the pickle library, pickle.dump() https://pythontic.com/modules/pickle/dump
-#   - pickle.dump() is a byte stream serializer. It is taking the python Object, face_data and serializing it as a series of bytes into a location in memory
+# + Synopsis
+#   - pickle.dump() is a byte stream serializer. It is taking the python Object, face_data and serializing it as a series of bytes into a location in memory.
 #   - In our case, the stream is our python object face_data, taking it's two array elements [ known_face_encodings, known_face_metadata ] and putting them into var face_data_file
 #
-# 
+# + Main function
+#   - This function is used twice in the main function to save all captured faces to disk periodically, and on program exit.
 
 def save_known_faces():
     with open("known_faces.dat", "wb") as face_data_file:
