2cac184879f96f4bc25e5b73153ab2b8b5c56f99
Ken <31672977+kennethcole5@users.noreply.github.com>
2021-11-05 18:27:47 -0500

    Modularized function 2 : breaking down tomorrow

---
+++ Function 2 : LoadingKnownFaces.py
@@ -0,0 +1,12 @@
+# Working on this tomorrow
+
+def load_known_faces():
+    global known_face_encodings, known_face_metadata
+
+    try:
+        with open("known_faces.dat", "rb") as face_data_file:
+            known_face_encodings, known_face_metadata = pickle.load(face_data_file)
+            print("Known faces loaded from disk.")
+    except FileNotFoundError as e:
+        print("No previous face data found - starting with a blank known face list.")
+        pass
