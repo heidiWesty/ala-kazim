8cbcf018ef2a8b43fb4449b1fb62a1836e4fb99a
Heidi Westenhiser <60275314+heidiWesty@users.noreply.github.com>
2021-12-07 20:21:15 -0600

    minor home changes

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,7 +3,7 @@ import alakazim from "../Images/Alakazam_1.png";
 import datapic from "../Images/data.jpg";
 import face from "../Images/FaceRecognition.png";
 import graph from "../Images/graph.jpg";
-import purple from "../Images/purple.jpg";
+import clock from "../Images/clock.jpg";
 import "./styles.css";
 import Login from "./Components/Login";
 import Navbar from "react-bootstrap/Navbar";
@@ -12,17 +12,18 @@ import Nav from "react-bootstrap/Nav";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import HzSeparator from "./Components/HzSeparator";
+import VtSeparator from "./Components/VtSeparator";
 
 export default function Home() {
   return (
     <div className="homeDiv">
       <Navbar bg="dark" variant="dark">
-        <Container style={{ paddingLeft: 25 }}>
-          <Navbar.Brand href="home">Home</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="about">About</Nav.Link>
-          </Nav>
-        </Container>
+        <Navbar.Brand style={{ paddingLeft: 25 }} href="home">
+          Home
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="about">About</Nav.Link>
+        </Nav>
         <Nav>
           <Nav.Link href="UserLogin" style={{ paddingRight: 25 }}>
             Login
@@ -33,45 +34,70 @@ export default function Home() {
         <img
           src={face}
           style={{
-            height: 450,
+            height: 425,
             width: "100%",
             objectFit: "cover",
           }}
         />
         <div className="text-block">
-          <p style={{ fontSize: "300%" }}>Ala-Kazim</p>
+          <p style={{ fontSize: "300%", fontFamily: "Segoe UI" }}>ALA-KAZIM</p>
           <p>
-            Using facial recognition to
-            <br />
-            track student attendance.
+            Using facial recognition <br />
+            to track student attendance.
           </p>
         </div>
       </div>
       <br />
       <br />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          marginLeft: "auto",
-          marginRight: "auto",
-          width: 1150,
-        }}
-      >
-        <Card style={{ width: "18rem", boxShadow: "10px 10px 9px grey" }}>
+      <div className="center-div">
+        <p style={{ fontFamily: "Segoe UI", fontSize: 35 }}>TECHNOLOGIES</p>
+      </div>
+      <p className="center-div" style={{ fontSize: 20 }}>
+        Some of the technologies used in the creation of this application:
+      </p>
+      <div className="homeImgsDiv">
+        <div>
+          <img className="homeImgs" src={clock} />
+          {/* <h2>React</h2> */}
+        </div>
+        <HzSeparator />
+        <div>
+          <img className="homeImgs" src={graph} />
+          {/* <h2>Jetson Nano</h2> */}
+        </div>
+        <HzSeparator />
+        <div>
+          <img className="homeImgs" src={datapic} />
+          {/* <h2 className="center-div">Firebase</h2> */}
+        </div>
+        {/* <Card style={{ width: "18rem", boxShadow: "10px 10px 9px grey" }}>
           <Card.Img
             variant="top"
-            src={purple}
+            src={clock}
             style={{ width: "100%", height: "15vw", objectFit: "cover" }}
           />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Saves Time</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Once a student image is captured, their attendance status is
+              immediately sent to a csv file.
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <HzSeparator />
+
+        <Card style={{ width: "18rem", boxShadow: "10px 10px 9px grey" }}>
+          <Card.Img
+            variant="top"
+            src={graph}
+            style={{ width: "100%", height: "15vw", objectFit: "cover" }}
+          />
+          <Card.Body>
+            <Card.Title>Data Collection</Card.Title>
+            <Card.Text>
+              Statistics of attendance can quickly be displayed visually after
+              each class period.
+            </Card.Text>
           </Card.Body>
         </Card>
         <HzSeparator />
@@ -88,26 +114,8 @@ export default function Home() {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
           </Card.Body>
-        </Card>
-        <HzSeparator />
-
-        <Card style={{ width: "18rem", boxShadow: "10px 10px 9px grey" }}>
-          <Card.Img
-            variant="top"
-            src={graph}
-            style={{ width: "100%", height: "15vw", objectFit: "cover" }}
-          />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        </Card> */}
       </div>
     </div>
   );
