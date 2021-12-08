5eb3cf35ce9a6e0c448a807c7b21e4e7bac76212
heidiWesty <heidi.diggs@selu.edu>
2021-12-07 19:33:44 -0600

    Revert "vtseparator"
    This reverts commit 908f34712797932075abe8e5bd3a9471452b3848.

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -12,7 +12,6 @@ import Nav from "react-bootstrap/Nav";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import HzSeparator from "./Components/HzSeparator";
-import VtSeparator from './Components/VtSeparator';
 
 export default function Home() {
   return (
@@ -31,7 +30,6 @@ export default function Home() {
       <div style={{ position: "relative" }}>
         <img
           src={face}
-          alt=''
           style={{
             height: 450,
             width: "100%",
@@ -49,7 +47,6 @@ export default function Home() {
       </div>
       <br />
       <br />
-      <VtSeparator/>
       <div
         style={{
           display: "flex",
